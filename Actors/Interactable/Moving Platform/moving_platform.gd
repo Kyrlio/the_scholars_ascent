e25e3ecf8c11cf82a0574741dafbfea404193860
bureau <cedricroussel2002@gmail.com>
2026-06-05 17:04:12 +0200

    things...

--- Actors/Interactable/Moving Platform/moving_platform.gd
+++ Actors/Interactable/Moving Platform/moving_platform.gd
@@ -4,9 +4,17 @@ extends Node2D
 @export var offset: Vector2 = Vector2(320, 0)
 @export var duration: float = 10.0
 
+@export_group("Rail Textures")
+@export var rail_start: Texture2D
+@export var rail_middle: Texture2D
+@export var rail_end: Texture2D
+
+
 @onready var platform_body: AnimatableBody2D = $PlatformBody
 
 func _ready() -> void:
+	_generate_rails()
+	
 	var tween := create_tween().set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
 	
 	tween.set_loops().set_parallel(false)
@@ -14,3 +22,35 @@ func _ready() -> void:
 	
 	tween.tween_property(platform_body, "position", offset, duration / 2.0).from_current()
 	tween.tween_property(platform_body, "position", Vector2.ZERO, duration / 2.0)
+
+
+func _generate_rails() -> void:
+	var tile_size: int = 16
+	var distance: float = offset.length()
+	var direction: Vector2 = offset.normalized()
+	
+	var num_tiles: int = int(distance / tile_size)
+	
+	var rail_container = Node2D.new()
+	rail_container.position.y += 2
+	add_child(rail_container)
+	move_child(rail_container, 0)
+	
+	for i in range(num_tiles + 1):
+		var sprite = Sprite2D.new()
+		
+		if i == 0:
+			sprite.texture = rail_start
+		elif i == num_tiles:
+			sprite.texture = rail_end
+		else:
+			sprite.texture = rail_middle
+		
+		sprite.rotation = direction.angle()
+		
+		sprite.position = direction * (i * tile_size)
+		
+		rail_container.add_child(sprite)
+	
+	
+	
--- Actors/Interactable/Sign/sign.gd
+++ Actors/Interactable/Sign/sign.gd
@@ -3,44 +3,18 @@ class_name Sign
 
 @export var sign_text: String = "Press A to jump"
 
-@onready var interact_sprite: Sprite2D = $InteractSprite
-@onready var interact_animation_player: AnimationPlayer = $InteractSprite/AnimationPlayer
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var label: Label = $TextVisuals/Label
 
-var is_interacted: bool = false
 var player_in_zone: Player = null
 
 
 func _ready() -> void:
 	label.text = sign_text
 	body_entered.connect(_on_body_entered)
-	body_exited.connect(_on_body_exited)
-
-
-func _unhandled_input(event: InputEvent) -> void:
-	if player_in_zone and event.is_action_pressed("interact") and not is_interacted:
-		interact_sign()
-		get_viewport().set_input_as_handled()
-
-
-func interact_sign() -> void:
-	interacted(true)
-	interact_animation_player.play("show_text")
 
 
 func _on_body_entered(body: Node2D) -> void:
-	if body is Player and not is_interacted:
-		player_in_zone = body
-		interact_animation_player.play("show")
-
-
-func _on_body_exited(body: Node2D) -> void:
 	if body is Player:
-		player_in_zone = null
-		
-		if not is_interacted:
-			interact_animation_player.play("hide")
-
-
-func interacted(toggled: bool) -> void:
-	is_interacted = toggled
+		player_in_zone = body
+		animation_player.play("show_text")
